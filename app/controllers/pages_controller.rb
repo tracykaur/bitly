8bc8958ffd29fa40aa32b85edb89d16b69b878d2
tracykaur <tracykaur@yahoo.com>
2017-10-23 17:47:36 +1000

    created shorten url page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,27 @@
 class PagesController < ApplicationController
+
+  def home
+  end
+
+  def shorten
+    @link = Link.new (link_params)
+    if @link.save
+      @short_url = @link.id.to_s(36)
+      render :success
+
+    end
+  end
+
+  def redirect
+    id = params[:hash].to_i(36)
+    redirect_to Link.find(id).long_url
+  end
+
+  private
+  def link_params
+    params.require(:shorten).permit(:long_url)
+  end
+
+
+
 end
